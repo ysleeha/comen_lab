2006b1d7972eee90cc8b15991203ba6c80bf0976
ysleeha <leeus8520@gmail.com>
2024-02-04 20:49:18 +0900

    modify fourth commit

--- practice_4/practice_4.srcs/sources_1/new/uart_receiv.sv
+++ practice_4/practice_4.srcs/sources_1/new/uart_receiv.sv
@@ -1,4 +1,4 @@
-
+/*
 `timescale 1ns / 1ps
 
 module uart_receiv
@@ -152,32 +152,29 @@ module uart_receiv
     end
 
 endmodule
+*/
 
+`timescale 1ns / 1ps
 
-/*
 module uart_receiv
 (
     input logic clk,  // 1Khz  (주기 : 0.1ms) 
     input logic rst,
-    input logic rx,
-    input logic cnt,
+    input logic rx,   // 10bit 데이터가 들어옴. 
     
     output logic [9:0] data_buffer // 수신된 문자열 데이터 버퍼
 );
-//    logic en_sig; 
-//    assign en_sig = en;
+
+    //logic [9:0] data;
+    //logic [4:0] cnt_data;
     
     // 10ms, 100클럭 대기한다. .
     localparam WAIT_TICKS = 100; 
     logic [7:0] cnt_wait_ticks;
     
-//    logic rx_reg;
-//    assign rx_reg = rx; 
-    
     logic [3:0] cnt_data_bit;           // 데이터 비트 카운터
     logic [9:0] data_receive;           // 현재 수신된 데이터
-    
-    //logic [0:9] data_buffer;            // 수신된 문자열 데이터 버퍼
+
     logic [3:0] cnt_buffer_index = 0;   // 버퍼 저장 카운터 
     logic string_received = 0; // 버퍼 수신 상태 
     
@@ -212,7 +209,7 @@ module uart_receiv
             end 
             
             WAITS: begin 
-                if(cnt_wait_ticks == WAIT_TICKS) begin 
+                if(~rx) begin 
                     next_state = START_BIT;
                 end
             end
@@ -222,13 +219,13 @@ module uart_receiv
             end
             
             DATA_BITS: begin
-                if(cnt_data_bit >= 8) begin //9) begin 
+                if(cnt_data_bit >= 9) begin 
                     next_state = STOP_BIT;
                 end
             end
             
             STOP_BIT: begin
-                if(cnt_data_bit >= 9) begin //10) begin 
+                if(cnt_data_bit >= 10) begin 
                     next_state = DATA_TRANS;
                 end
             end
@@ -243,9 +240,6 @@ module uart_receiv
 
     // 데이터 수신 및 처리 로직
     always @(posedge clk or negedge rst) begin
-        
-        data_receive <= rx;
-        
         if (!rst) begin
             cnt_wait_ticks <= 0;
             
@@ -254,6 +248,8 @@ module uart_receiv
             
             cnt_buffer_index <= 0;
             data_buffer <= 0;
+            
+            string_received <= 0;
         end 
         else begin
             case (next_state)
@@ -274,37 +270,24 @@ module uart_receiv
                 
                 START_BIT: begin
                     cnt_wait_ticks <= 0;
-                    if(cnt==0) begin  
-                        data_receive[cnt_data_bit] <= 0;
-                        //cnt_data_bit <= cnt_data_bit + 1;
-                    end
-                    else begin 
-                    end
-                    //rx <= 0; // 데이터 비트 수신
+                    
+                    cnt_data_bit <= cnt_data_bit + 1;
+                    data_receive[cnt_data_bit] <= rx; // 데이터 비트 수신 [0bit]
                 end
                 
                 DATA_BITS: begin
-                    
-                    if(en==1) begin 
-                        // 이 rx를 어떻게 해야 H 라는 데이터를 집어넣을 수 있을까????
-                        data_receive[cnt_data_bit] <= rx; // 데이터 비트 수신
-                        cnt_data_bit <= cnt_data_bit + 1;
-                    end
-                    else begin
-                    end
+                    cnt_data_bit <= cnt_data_bit + 1;
+                    // 이 rx를 어떻게 해야 H 라는 데이터를 집어넣을 수 있을까????
+                    data_receive[cnt_data_bit] <= rx; // 데이터 비트 수신 [1bit~8bit]
                 end
                 
                 STOP_BIT: begin
-                    if(en==1) begin 
-                        data_receive[cnt_data_bit] <= 0; // 데이터 비트 수신
-                        cnt_data_bit <= cnt_data_bit + 1;
-                    end
-                    else begin
-                    end
+                    cnt_data_bit <= cnt_data_bit + 1;
+                    data_receive[cnt_data_bit] <= rx; // 데이터 비트 수신 [9bit] 
                 end
                 
-                DATA_TRANS : begin 
-                    data_buffer[cnt_buffer_index] <= data_receive[cnt_buffer_index];
+                DATA_TRANS : begin  
+                    data_buffer[cnt_buffer_index] <= data_receive[cnt_buffer_index]; 
                     cnt_buffer_index <= cnt_buffer_index + 1;
                     // "H" 문자열 길이 확인
                     if (cnt_buffer_index == 10) begin
@@ -326,5 +309,3 @@ module uart_receiv
     end
 
 endmodule
-
-*/
